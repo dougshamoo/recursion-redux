5764f23bbc3a3ffaa933412e28eea53a691f42e1
Doug Shamoo <doug.shamoo@gmail.com>
2015-08-17 18:52:17 -0700

    Started implementing parseJSON, doesn't work yet

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,37 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
-var parseJSON = function(json) {
-  // your code goes here
+var parseJSON = function(json, idx) {
+  // function for getting the next ch
+  
+  idx = idx || 0;
+  var ch = json[idx];
+
+  var next = function() {
+    idx++;
+    ch = json[idx];
+  }
+
+  // handle arrays []
+  
+
+  // handle objects {}
+
+
+  // handle strings " "
+
+ 
+  // handle booleans (have to spell out)
+
+
+  // handle numbers
+  if (ch >= '0' && ch <= '9') {
+    
+  }
+
+
+  // null / undefined (have to spell out)
+
+  // checks ch and calls appropriate function
+  
 };
